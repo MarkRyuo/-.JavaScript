03542c17cc98037932bd383fc6533bc9e10a2bfa
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-01 19:39:11 +0800

    SUBJECT: BODY

--- Random/App1.js
+++ Random/App1.js
@@ -4,10 +4,19 @@ let fname = "Ryoda" ; // * Let can declare again
 const age = 21 ; //* const can't declare
 
 //* Loop 
-let lname = ""
+let lname = "" ;
 
 for(let x = 0 ; x < fname.length; x++) {
     lname += x 
 }
 
-console.log(x)
+console.log(lname)
+
+//* JavaScript supports different kinds of loops:
+/* 
+    for - loops through a block of code a number of times
+    for/in - loops through the properties of an object
+    for/of - loops through the values of an iterable object
+    while - loops through a block of code while a specified condition is true
+    do/while - also loops through a block of code while a specified condition is true
+*/
