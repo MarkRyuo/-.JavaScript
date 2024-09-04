cfd1f4ac37f0d3d1aa7c39cf06b98464b38119d3
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-04 08:54:06 +0800

    Subject: Body

--- Fetch/scriptt.js
+++ Fetch/scriptt.js
@@ -1,6 +1,6 @@
 
 fetch('https://pokeapi.co/api/v2/pokemon/pikachu') 
-    .then(response => console,json())
+    .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error(error));
 
